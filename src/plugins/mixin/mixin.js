ed7f275ab37cbdd17cb759d46fc93bf1c23940e2
gaocc <15957108449@163.com>
2022-12-16 17:24:03 +0800

    fix 增加通用流文件下载

--- src/plugins/mixin/mixin.js
+++ src/plugins/mixin/mixin.js
@@ -206,6 +206,29 @@ export default {
       for (let k in o)
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
       return fmt;
-    }
+    },
+
+    // 下载流通用处理方式，如果是word则设置为msword，excel为excel
+    download(res, type = 'application/pdf;chartset=UTF-8', filename) {
+      // 创建blob对象，解析流数据
+      const blob = new Blob([res], {
+        // 如何后端没返回下载文件类型，则需要手动设置：type: 'application/pdf;chartset=UTF-8' 表示下载文档为pdf，如果是word则设置为msword，excel为excel
+        type: type
+      })
+      const a = document.createElement('a')
+      // 兼容webkix浏览器，处理webkit浏览器中href自动添加blob前缀，默认在浏览器打开而不是下载
+      const URL = window.URL || window.webkitURL
+      // 根据解析后的blob对象创建URL 对象
+      const herf = URL.createObjectURL(blob)
+      // 下载链接
+      a.href = herf
+      // 下载文件名
+      a.download = filename
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+      // 在内存中移除URL 对象
+      window.URL.revokeObjectURL(herf)
+    },
   }
 };
